1010f54e029f44881472955e7613e0570546d4e1
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-06-02 13:11:40 +1200

    Created interactionCreate.js event handler for cmds

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,6 +3,29 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction, bot) {
+        if (await interaction.isChatInputCommand()) {
+            // If slash command
+            // Get command path with name
+            const path = await this.commandPathsMap.get(interaction.commandName);
+            const command = await require('../' + path);
 
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+    
+            try {
+                await command.execute(interaction, bot);
+            } catch (error) {
+                console.error(error);
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
+            }
+        } else {
+            // If other (button, etc)
+        }
     },
 };
